1f51dcd4c098c51fa380a9a634e589da135a93e5
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-25 18:43:14 +0200

    Empezando con las tuplas

--- tuplas.py
+++ tuplas.py
@@ -1,1 +1,13 @@
 # Tuplas -> Son inmutables, su valor NO puede cambiar, a diferencia de las listas las tuplas se definen con parentesis ()
+
+car_brands = ('seat', 'audi', 'bmw')
+
+for car in car_brands:
+    print(car.title())
+
+# Como los valores son inmutables, para modificar un valor hay que reasignar los valores a la tupla
+
+car_brands = ('seat', 'audi', 'honda')
+
+for car in car_brands:
+    print(car.title())
