ef514b5f43897358de9d15c152d628ac2bcc32f6
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-25 18:19:24 +0200

    Subiendo mas  recursos

---
+++ partir_listas.py
@@ -0,0 +1,20 @@
+# Partir listas
+
+numbers = list(range(1, 31))
+
+# Trozo de la lista del 1 al 5
+numbers_slide_1 = numbers[0:5]
+print(numbers_slide_1)
+
+# Trozo de la lista del 11 al 15
+numbers_slide_2 = numbers[10:15]
+print(numbers_slide_2)
+
+# Trozo de la lista del 1 al 10
+numbers_slide_3 = numbers[:10]
+print(numbers_slide_3)
+
+# Trozo de la lista del 20 al 30
+numbers_slide_4 = numbers[19:]
+print(numbers_slide_4)
+
