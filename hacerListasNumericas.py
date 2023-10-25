2c1dc272907baa1861cc6e82f50c67e27e49b985
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-25 18:10:09 +0200

    Subiendo mas  recursos

--- hacerListasNumericas.py
+++ hacerListasNumericas.py
@@ -5,9 +5,17 @@ for value in range(1, 10):
 for value in range(7):
     print(value) # 0...6
 
+for value in range(1, 21):
+    print(value)
+
 # Utilizando la funcion list() podemos crear una lista de numeros
 number_list = list(range(1, 13))
 print(number_list)
+
+million_list = list(range(1, 1000001))
+print(min(million_list))
+print(max(million_list))
+print(sum(million_list))
 
 # Pasando un tercer parametro a la funcion range() podriamos guardar una lista de numeros pares o impares
 even_numbers = list(range(2, 10, 2)) # Este tercer valor sumara de 2 en 2 empezando en 2
@@ -28,3 +36,8 @@ print(f"sum of all numbers in the list -> {sum(squares)}")
 
 # Listas por compresion
 
+squares_compression_list = [value ** 2  for value in range(1, 11)]
+print(squares_compression_list)
+
+multiples_of_3 = [value ** 3 for value in range(3, 31)]
+print(multiples_of_3)
